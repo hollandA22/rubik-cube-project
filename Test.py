335ec98956e88325bb6ec6408bf2245dd8bbed23
hollandA22 <alexholla6@gmail.com>
2023-02-13 23:44:38 -0600

    update 1.0

--- Test.py
+++ Test.py
@@ -1,39 +1,59 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import tkinter as tk
 
-def plot_rubiks_cube(front, back, up, down, left, right):
-    """
-    Plot a 3x3 Rubik's cube using 6 lists for each side.
-    Each list should contain 9 elements, representing the color of each square.
-    """
-    # Combine the 6 lists into a single 3D array
-    cube = np.zeros((3, 3, 6))
-    cube[:, :, 0] = front.reshape((3, 3))
-    cube[:, :, 1] = back.reshape((3, 3))
-    cube[:, :, 2] = up.reshape((3, 3))
-    cube[:, :, 3] = down.reshape((3, 3))
-    cube[:, :, 4] = left.reshape((3, 3))
-    cube[:, :, 5] = right.reshape((3, 3))
-    
-    # Define the x, y, and z coordinates for each face of the cube
-    x = np.array([0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0])
-    y = np.array([0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1])
-    z = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
+class Cube3D:
+    def __init__(self, master, size):
+        self.master = master
+        self.canvas = tk.Canvas(master, width=size, height=size, bg="white")
+        self.canvas.pack()
 
-    # Define the colors for each face
-    colors = np.zeros((16, 4))
-    colors[:, :3] = cube[x, y, z]
-    colors[:, 3] = 1
+        # Define the 3D coordinates of each vertex of the cube
+        self.vertices = [[0, 0, 0], [0, 1, 0], [1, 1, 0], [1, 0, 0],
+                         [0, 0, 1], [0, 1, 1], [1, 1, 1], [1, 0, 1]]
 
-    # Plot the cube
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_xlim(0, 3)
-    ax.set_ylim(0, 3)
-    ax.set_zlim(0, 3)
-    ax.set_xticks(np.arange(0, 4))
-    ax.set_yticks(np.arange(0, 4))
-    ax.set_zticks(np.arange(0, 4))
-    ax.voxels(cube, facecolors=colors, edgecolor='k')
-    plt.show()
+        # Define the edges of the cube
+        self.edges = [[0, 1], [1, 2], [2, 3], [3, 0],
+                      [4, 5], [5, 6], [6, 7], [7, 4],
+                      [0, 4], [1, 5], [2, 6], [3, 7]]
+
+        # Draw the cube
+        self.draw_cube()
+
+        # Initialize the mouse position and rotation angles
+        self.last_x, self.last_y = None, None
+        self.theta, self.phi = 0, 0
+
+        # Bind mouse events to the canvas
+        self.canvas.bind("<Button-1>", self.on_left_mouse_down)
+        self.canvas.bind("<ButtonRelease-1>", self.on_left_mouse_up)
+        self.canvas.bind("<B1-Motion>", self.on_left_mouse_drag)
+
+    def draw_cube(self):
+        # Project the 3D coordinates onto the 2D canvas
+        points = []
+        for vertex in self.vertices:
+            x = vertex[0] * 100 + 150
+            y = vertex[1] * 100 + 150
+            z = vertex[2] * 100 + 150
+            x, y = self.rotate_point(x, y, z)
+            points.append((x, y))
+
+        # Draw the edges of the cube
+        for edge in self.edges:
+            x1, y1 = points[edge[0]]
+            x2, y2 = points[edge[1]]
+            self.canvas.create_line(x1, y1, x2, y2)
+
+    def on_left_mouse_down(self, event):
+        self.last_x, self.last_y = event.x, event.y
+
+    def on_left_mouse_up(self, event):
+        self.last_x, self.last_y = None, None
+
+    def on_left_mouse_drag(self, event):
+        if self.last_x is not None and self.last_y is not None:
+            dx = event.x - self.last_x
+            dy = event.y - self.last_y
+            self.rotate_cube(dx, dy)
+            self.canvas.delete("all")
+            self.draw_cube()
+        self.last_x, self.last_y = event.x, event.y
